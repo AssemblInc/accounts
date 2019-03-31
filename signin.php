b84c43b3cc589b2c123f137b8537d97d6cf893b1
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2019-04-01 01:39:08 +0200

    replaced modal window with redirect

--- signin.php
+++ signin.php
@@ -21,6 +21,7 @@
         <p><small>ORCID provides a persistent identifier &ndash; an ORCID iD &ndash; that distinguishes you from other researchers and a mechanism for linking your research outputs and activities to your iD. Learn more at <a href="https://orcid.org">orcid.org</a>.</small></p>
         <button id="orcid-btn"><img id="orcid-id-icon" src="https://orcid.org/sites/default/files/images/orcid_24x24.png" width="24" height="24" alt="ORCID iD icon"/>Register or Connect your ORCID iD</button>
         <script>
+        /*
         var oAuthWindow;
         document.getElementById("orcid-btn").addEventListener("click", function() {
             var w = 500;
@@ -28,6 +29,10 @@
             var left = (window.innerWidth/2)-(w/2);
             var top = (window.innerHeight/2)-(h/2);
             oAuthWindow = window.open("https://orcid.org/oauth/authorize?client_id=***REMOVED_ORCID_CLIENT_ID***&response_type=code&scope=/authenticate&redirect_uri=https://accounts.assembl.science/callback/orcid/", "_blank", "toolbar=no, scrollbars=yes, width="+w+", height="+h+", top="+top+", left="+left);
+        });
+        */
+        document.getElementById("orcid-btn").addEventListener("click", function() {
+            window.location.href = "https://orcid.org/oauth/authorize?client_id=***REMOVED_ORCID_CLIENT_ID***&response_type=code&scope=/authenticate&redirect_uri=https://accounts.assembl.science/callback/orcid/";
         });
         </script>
     </body>
