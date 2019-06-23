0e033716e74f82cc4d37d21d2c5e0dae66c9c9cb
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2019-06-23 17:52:49 +0200

    added basic orcid connection support

---
+++ callback/connect-cb.php
@@ -0,0 +1,71 @@
+<?PHP
+    // error_reporting(E_ALL); ini_set('display_errors', 1);
+
+    require_once("../import/sessionstart.php");
+
+    if (!isset($_GET["s"]) || empty($_GET["s"])) {
+        die("Provide the service to connect to with the parameter s in the URL.");
+    }
+    else {
+        switch($_GET["s"]) {
+            case "orcid": {
+                if (isset($_GET["code"]) && !empty($_GET["code"])) {
+                    $ch = curl_init();
+                    curl_setopt($ch, CURLOPT_URL, "https://orcid.org/oauth/token");
+                    curl_setopt($ch, CURLOPT_POST, true);
+                    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
+                        'client_id' => '***REMOVED_ORCID_CLIENT_ID***',
+                        'client_secret' => '***REMOVED_ORCID_CLIENT_SECRET***',
+                        'grant_type' => 'authorization_code',
+                        'code' => $_GET["code"],
+                        'redirect_uri' => 'https://accounts.assembl.ch/callback/connect-cb/?s=orcid'
+                    )));
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                    $response = curl_exec($ch);
+                    curl_close($ch);
+                    if ($response !== false) {
+                        $jsonResponse = json_decode($response, true);
+                        if (!array_key_exists("error", $jsonResponse)) {
+                            $expiresOn = date("Y-m-d H:i:s", time() + intval($jsonResponse["expires_in"]));
+
+                            require_once("../import/assembldb.php");
+                            $connection = AssemblDB::getAccountsConnection();
+                            
+                            $sql = "UPDATE `users`.`orcid` SET `orcid_id`='".AssemblDB::makeSafe($jsonResponse["orcid"], $connection)."', `orcid_token_type`='".AssemblDB::makeSafe($jsonResponse["token_type"], $connection)."', `orcid_access_token`='".AssemblDB::makeSafe($jsonResponse["access_token"], $connection)."', `orcid_refresh_token`='".AssemblDB::makeSafe($jsonResponse["refresh_token"], $connection)."', `orcid_expires_on`='".AssemblDB::makeSafe($expiresOn, $connection)."', `orcid_scope`='".AssemblDB::makeSafe($jsonResponse["scope"], $connection)."', `connected_on`=CURRENT_TIMESTAMP() WHERE `uid`='".AssemblDB::makeSafe($_SESSION["userdata"]["uid"], $connection)."' LIMIT 1";
+                            $result = mysqli_query($connection, $sql);
+                            
+                            if ($result !== false) {
+                                if (mysqli_affected_rows($connection) > 0) {
+                                    header("Location: /settings/");
+                                    die();
+                                }
+                                else {
+                                    die("Could not add connection details to database");
+                                }
+                            }
+                            else {
+                                die("SQL error: ".mysqli_error($connection));
+                            }
+                        }
+                        else {
+                            die($jsonResponse["error_description"]);
+                        }
+                    }
+                    else {
+                        die("Could not trade authorization code for access token");
+                    }
+                }
+                else if (isset($_GET["error"])) {
+                    die($_GET["error_description"]);
+                }
+                else {
+                    die("GET parameter 'code' but also 'error' is not set");
+                }
+                break;
+            }
+            default: {
+                die("Unknown service");
+            }
+        }
+    }
+?>
---
+++ connect.php
@@ -0,0 +1,64 @@
+<?PHP
+    require("import/sessionstart.php");
+    require_once("import/continuer.php");
+    require("api/requirelogin.php");
+
+    if (!isset($_GET["s"]) || empty($_GET["s"])) {
+        header("Location: /settings/");
+        die();
+    }
+
+    switch($_GET["s"]) {
+        case "orcid":
+            $s = "orcid";
+            $sName = "ORCID";
+            $sExp = 'ORCID provides a persistent identifier – an ORCID iD – that distinguishes you from other researchers and a mechanism for linking your research outputs and activities to your iD. Learn more at <a target="_blank" href="https://orcid.org">orcid.org</a>.';
+            $sBtn = '<img src="https://orcid.org/sites/default/files/images/orcid_24x24.png" width="24" height="24" alt="ORCID iD icon" style="vertical-align: middle;"><span style="vertical-align: middle; line-height: 24px;">Connect your ORCID iD</span>';
+            $sUrl = "https://orcid.org/oauth/authorize?client_id=***REMOVED_ORCID_CLIENT_ID***&response_type=code&scope=/authenticate&redirect_uri=https://accounts.assembl.ch/callback/connect-cb/?s=orcid";
+            break;
+        default:
+            header("Location: /settings/");
+            die();
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Connect <?PHP echo $sName; ?> to your Assembl account</title>
+        <base href="https://accounts.assembl.ch/" />
+        <link rel="stylesheet" href="/loginstyles.css" />
+		<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" type="image/ico" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#193864" />
+        <script>
+        function getParameterByName(name, url) {
+            if (!url) url = window.location.href;
+            name = name.replace(/[\[\]]/g, "\\$&");
+            var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)", "i"),
+            results = regex.exec(url);
+            if (!results) return null;
+            if (!results[2]) return '';
+            return decodeURIComponent(results[2].replace(/\+/g, " "));
+        }
+        </script>
+    </head>
+    <body>
+        <div class="signin-table">
+            <div class="signin-table-cell">
+                <div class="signin-table-cell-content">
+                    <div style="display: none;" id="loading">
+                        <img class="loading-svg" src="import/loading.svg" />
+                    </div>
+                    <script src="/import/loader.js"></script>
+                    <h1>Assembl</h1>
+                    <h2>Connect <?PHP echo $sName; ?> to your Assembl account</h2>
+                    <hr />
+                    <p><small><?PHP echo $sExp; ?></small></p>
+                    <a class="assembl-btn full-width no-height-limit" href="<?PHP echo $sUrl; ?>"><?PHP echo $sBtn; ?></a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
